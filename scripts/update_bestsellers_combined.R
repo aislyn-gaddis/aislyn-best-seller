67cf4991711b0a628b77772ad23644cf00e6c817
aislyn-gaddis <143017359+aislyn-gaddis@users.noreply.github.com>
2025-03-13 12:21:14 -0500

    Update update_bestsellers_combined.R

--- scripts/update_bestsellers_combined.R
+++ scripts/update_bestsellers_combined.R
@@ -16,12 +16,23 @@ if (file.exists(data_file)) {
   bestsellers <- tibble()  # If no file exists, start fresh
   last_date <- as.Date("2011-02-13")  # 🔧 Change this if your start date is different
 }
-print(last_date)
 
-# Calculate next week's date (ensures we're only scraping new data)
-current_date <- Sys.Date()
-next_sunday <- current_date + (8 - wday(current_date))
-print(next_sunday)
+# Calculate next week's date
+current_date = Sys.Date()
+day_of_week = wday(current_date)
+ 
+# note: the list for the following week is published on Wednesdays at 7pm EST
+
+if (day_of_week == 1) {
+  next_sunday = current_date + 7
+} else if (day_of_week >= 2 & day_of_week <= 3) {
+   days_until_sunday = 8 - day_of_week
+   next_sunday = current_date + days_until_sunday # end_date is equal to the sunday after the current_date
+} else if (day_of_week >= 4 & day_of_week <= 7) {
+  days_until_sunday = 8 - day_of_week
+  next_sunday = current_date + days_until_sunday + 7 # end_date is equal to 2 sundays after the current_date
+}
+print(next_sunday) # for debugging if next_sunday value is correct
 
 # Check if data needs updating
 if (next_sunday > last_date) {
